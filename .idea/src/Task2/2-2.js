4966250e1e19699b4ef9647c84bf23a0501dcbcc
Roman Taras <taras-chemist@ukr.net>
2020-04-07 11:16:58 +0300

    JS - lesson 2 - home tasks

---
+++ .idea/src/Task2/2-2.js
@@ -0,0 +1,17 @@
+var styles = ['Dzhaz','Blues'];
+console.log(styles);
+styles.push('Rock-n-Roll');
+console.log(styles);
+styles.splice(1,1,'Classic');
+console.log(styles);
+console.log(styles.shift());
+console.log(styles);
+styles.unshift('Rap','Reggi');
+console.log(styles);
+
+
+
+
+
+
+
